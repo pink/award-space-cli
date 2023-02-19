bd9c2fbb23293ec3f9a8a669abe66cd85ca57c43
Sean Kallungal <sean.kallungal@gmail.com>
2023-02-19 15:28:55 -0500

    tabs -> spaces

--- source/FlightTable.tsx
+++ source/FlightTable.tsx
@@ -5,57 +5,57 @@ import {findOpenSeats} from './FlightAPI';
 import {Box, Text} from 'ink';
 
 interface FlightTableProps {
-	origin: string;
-	destination: string;
-	numSeats: number;
+  origin: string;
+  destination: string;
+  numSeats: number;
 }
 
 type FlightTableSchema = {
-	Date: string;
-	Airlines: string;
-	'# of Seats in Business': string;
-	'# of Seats in First': string;
+  Date: string;
+  Airlines: string;
+  '# of Seats in Business': string;
+  '# of Seats in First': string;
 };
 
 const FlightTable = (props: FlightTableProps) => {
-	const [flightData, setFlightData] = useState<FlightTableSchema[]>([]);
+  const [flightData, setFlightData] = useState<FlightTableSchema[]>([]);
 
-	useEffect(() => {
-		const loadFlightData = async () => {
-			const data = await findOpenSeats(
-				props.origin,
-				props.destination,
-				props.numSeats
-			);
+  useEffect(() => {
+    const loadFlightData = async () => {
+      const data = await findOpenSeats(
+        props.origin,
+        props.destination,
+        props.numSeats
+      );
 
-			const formattedData: FlightTableSchema[] = data.map(d => {
-				return {
-					Date: d.Date,
-					Airlines: d.JAirlines,
-					'# of Seats in Business': String(d.JRemainingSeats),
-					'# of Seats in First': String(d.FRemainingSeats),
-				};
-			});
-			setFlightData(formattedData);
-		};
+      const formattedData: FlightTableSchema[] = data.map(d => {
+        return {
+          Date: d.Date,
+          Airlines: d.JAirlines,
+          '# of Seats in Business': String(d.JRemainingSeats),
+          '# of Seats in First': String(d.FRemainingSeats),
+        };
+      });
+      setFlightData(formattedData);
+    };
 
-		loadFlightData();
-	}, []);
+    loadFlightData();
+  }, []);
 
-	const loadingDataComponent = (
-		<Text>
-			<Text color="green">
-				<Spinner type="dots" />
-			</Text>
-			{' Loading flight data...'}
-		</Text>
-	);
+  const loadingDataComponent = (
+    <Text>
+      <Text color="green">
+        <Spinner type="dots" />
+      </Text>
+      {' Loading flight data...'}
+    </Text>
+  );
 
-	return (
-		<Box>
-			{!flightData.length ? loadingDataComponent : <Table data={flightData} />}
-		</Box>
-	);
+  return (
+    <Box>
+      {!flightData.length ? loadingDataComponent : <Table data={flightData} />}
+    </Box>
+  );
 };
 
 export default FlightTable;
--- source/cli.tsx
+++ source/cli.tsx
@@ -5,38 +5,38 @@ import meow from 'meow';
 import App from './ui';
 
 const cli = meow(
-	`
-	Usage
-	  $ award-space-cli
+  `
+  Usage
+    $ award-space-cli
 
-	Options (all are optional):
-		--origin  Starting airport (e.g. JFK)
-		--destination  Destination airport (e.g. HND)
-		--numSeats  Number of seats to search for
+  Options (all are optional):
+    --origin  Starting airport (e.g. JFK)
+    --destination  Destination airport (e.g. HND)
+    --numSeats  Number of seats to search for
 
-	Examples
-	  $ award-space-cli --origin=JFK --destination=HND --numSeats=2
-	  Hello, Jane
+  Examples
+    $ award-space-cli --origin=JFK --destination=HND --numSeats=2
+    Hello, Jane
 `,
-	{
-		flags: {
-			origin: {
-				type: 'string',
-			},
-			destination: {
-				type: 'string',
-			},
-			numSeats: {
-				type: 'number',
-			},
-		},
-	}
+  {
+    flags: {
+      origin: {
+        type: 'string',
+      },
+      destination: {
+        type: 'string',
+      },
+      numSeats: {
+        type: 'number',
+      },
+    },
+  }
 );
 
 render(
-	<App
-		origin={cli.flags.origin}
-		destination={cli.flags.destination}
-		numSeats={cli.flags.numSeats}
-	/>
+  <App
+    origin={cli.flags.origin}
+    destination={cli.flags.destination}
+    numSeats={cli.flags.numSeats}
+  />
 );
